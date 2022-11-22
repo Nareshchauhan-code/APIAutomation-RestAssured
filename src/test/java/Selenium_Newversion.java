fb3f7fa5749128fa0c34ac05d9911781c1da0d15
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-11-22 20:31:36 +0530

    Done

---
+++ src/test/java/Selenium_Newversion.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Selenium_Newversion {
+
+    @Test
+    public void checkSeleniumVersion() throws IOException {
+
+      //  WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com/");
+
+
+        driver.quit();
+
+    }
+}
