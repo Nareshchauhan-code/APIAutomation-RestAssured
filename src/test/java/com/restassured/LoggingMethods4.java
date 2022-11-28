e30520535bce8192c3e2b1b385ee345c892388ae
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-11-29 00:51:54 +0530

    Log methods

---
+++ src/test/java/com/restassured/LoggingMethods4.java
@@ -0,0 +1,18 @@
+package com.restassured;
+
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+
+public class LoggingMethods3 {
+    @Test
+    public void testRestAssured() {
+
+        given().baseUri("https://api.getpostman.com").header("X-Api-Key", "PMAK-63590fbcd307b504237e1884-daff29b7948d5c5f5f8256c0cd232e7dca").
+                when().get("/workspaces").
+                then().log().ifError().assertThat().statusCode(200).body("workspaces.name", contains("My Workspace", "Team Workspace", "RestAssured"));
+
+    }
+}
