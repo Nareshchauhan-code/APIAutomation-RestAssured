4a3b22852410143bb425e7f8c2ba00c2e664f56d
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-11-29 20:07:00 +0530

    BDD to Non BDD

---
+++ src/test/java/com/restassured/BDDtonon_BDD.java
@@ -0,0 +1,66 @@
+package com.restassured;
+
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class BDDtonon_BDD {
+    RequestSpecification requestSpecification;
+
+    @BeforeClass
+    public void requestSpecificationReuse() {
+
+        requestSpecification = with().
+                baseUri("https://api.getpostman.com").
+                header("X-Api-Key", "PMAK-63590fbcd307b504237e1884-daff29b7948d5c5f5f8256c0cd232e7dca").log().all();
+
+
+    }
+
+    @Test
+    public void testRestAssured1() {
+        Response response=requestSpecification.get("/workspaces").
+                then().log().headers().extract().response();
+
+        assertThat(response.statusCode(),is(equalTo(200)));
+
+    }
+
+    @Test
+    public void testRestAssured2() {
+
+        Response response=requestSpecification.get("/workspaces").
+                then().log().headers().extract().response();
+
+        assertThat(response.statusCode(),is(equalTo(200)));
+
+
+    }
+
+    @Test
+    public void testRestAssured3() {
+        Response response=requestSpecification.get("/workspaces").
+                then().log().headers().extract().response();
+
+        assertThat(response.statusCode(),is(equalTo(200)));
+
+    }
+
+    @Test
+    public void testRestAssured4() {
+
+
+        Response response=requestSpecification.get("/workspaces").
+                then().log().headers().extract().response();
+
+        assertThat(response.statusCode(),is(equalTo(200)));
+
+    }
+}
